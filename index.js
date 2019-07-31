ad82d85a7b8be94c1b88ec183692dfe4e4ef1c7d
Lucas <ljwillow@hotmail.com>
2019-07-31 22:36:56 +0000

    changed index to ES6 shorthand

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
@@ -17,22 +16,22 @@ const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+module.exports =  {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
