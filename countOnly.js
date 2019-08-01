baa3f06496cd811427bf6924c5b82daefbf9fd91
Lucas <ljwillow@hotmail.com>
2019-08-01 02:26:19 +0000

    changeda var to a let in countOnly and updated some index in Read file

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,7 @@
 const countOnly = function (allItems, itemsToCount) {
   const result = {}
 
-  for (var item of allItems) {
+  for (let item of allItems) {
     if (itemsToCount[item] === true) {
       if (result[item]) result[item]++;
        else result[item] = 1;
